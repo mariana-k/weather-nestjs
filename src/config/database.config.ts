03a67bb3230791587c34792bd965be510f0507a4
Mariana Kyrkosh <mariana.kyrkosh@gmail.com>
2025-05-19 20:41:36 +0300

    fixed db config

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,14 +2,18 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Subscription } from '../entities/subscription.entity';
 
 export const databaseConfig: TypeOrmModuleOptions = {
-  type: 'mysql',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT, 10) || 3306,
-  username: process.env.DB_USERNAME || 'root',
-  password: process.env.DB_PASSWORD || 'root',
-  database: process.env.DB_DATABASE || 'weather_api',
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   entities: [Subscription],
   synchronize: process.env.NODE_ENV !== 'production',
   migrations: ['dist/migrations/*.js'],
   migrationsRun: true,
+  ssl: {
+    rejectUnauthorized: false
+  },
+  connectTimeoutMS: 10000, // 10 seconds
 }; 
